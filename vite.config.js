2ef560f935414eb026d37b3713a197e7d42818c9
Diovani <matosdiovani@gmail.com>
2024-03-02 19:31:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import '@fortawesome/react-fontawesome';
+import '@fortawesome/react-fontawesome';
 import "@fortawesome/free-brands-svg-icons";
 
 // https://vitejs.dev/config/
