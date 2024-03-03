a5566837fad69c0e44ba240105621bf9a9100eb9
Diovani <matosdiovani@gmail.com>
2024-03-03 18:11:48 -0300

    react-icons/fa

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from "@vitejs/plugin-react";
 import '@fortawesome/react-fontawesome';
 import "@fortawesome/free-brands-svg-icons";
 import "@fortawesome/free-regular-svg-icons"
+import "react-icons/fa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom", "@fortawesome/react-fontawesome", "@fortawesome/free-brands-svg-icons", "@fortawesome/free-regular-svg-icons"],
+      external: ["react-router-dom", "@fortawesome/react-fontawesome", "@fortawesome/free-brands-svg-icons", "@fortawesome/free-regular-svg-icons", 'react-icons/fa'],
     },
   },
 });
