c203abe174ec05521b951792b999e46630f0d042
Diovani <matosdiovani@gmail.com>
2024-03-03 17:58:54 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import '@fortawesome/react-fontawesome';
 import "@fortawesome/free-brands-svg-icons";
+import "@fortawesome/free-regular-svg-icons"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom", "@fortawesome/react-fontawesome", "@fortawesome/free-brands-svg-icons"],
+      external: ["react-router-dom", "@fortawesome/react-fontawesome", "@fortawesome/free-brands-svg-icons", "@fortawesome/free-regular-svg-icons"],
     },
   },
 });
