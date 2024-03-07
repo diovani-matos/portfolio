7e8093049cbf370f4a600f7ba4913e9e6f3c1551
Diovani <matosdiovani@gmail.com>
2024-03-07 11:33:52 -0300

    alteração no vite.config e delete do vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import "react-router-dom";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react-router-dom"
-      ],
-    },
-  },
+  
 });
